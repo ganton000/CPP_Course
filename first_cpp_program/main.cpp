24d59cfca9d40635ead00d1e9e484ee5baf48adb
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-02 00:26:37 -0500

    Configured tasks for new program and updated script

---
+++ first_cpp_program/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello";
+	std::cout << "World!" << std::endl;
+	std::cout << "Hello" << std::endl;
+	std::cout << "World!" << std::endl;
+}
