319fc84d847c219748d92fea4ed0b025721192e4
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-03 12:22:46 -0500

    Added first function .ccp attempt

---
+++ first_cpp_function/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+int addNumbers(int first_num, int second_num) {
+	int sum = first_num + second_num;
+	return sum;
+}
+
+int main() {
+
+	int firstNum = 9;
+	int secondNum = 12;
+
+	int expected_sum = firstNum + secondNum;
+
+	int sum = addNumbers(firstNum, secondNum);
+
+	std::cout << "The sum of " << firstNum << " and " << secondNum << " is " << expected_sum << std::endl;
+
+	std::cout << "The addNumbers function returns " << sum << " when adding " << firstNum << " and " << secondNum << std::endl;
+
+	return 0;
+}
