bfca9a77e73892566ad2f8a4d98608b4de3c5155
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-07 22:22:56 -0500

    Logging sizeof variables in bytes

--- number_systems/main.cpp
+++ number_systems/main.cpp
@@ -6,9 +6,21 @@ int main() {
 	int number3 = 0x0f; //Hexadecimal
 	int number4 = 0b00001111; //Binary - C++14
 
-	std::cout << "Number1 is : " << number1 << std::endl;
-	std::cout << "Number2 is : " << number2 << std::endl;
-	std::cout << "Number3 is : " << number3 << std::endl;
-	std::cout << "Number4 is : " << number4 << std::endl;
+	std::cout << "Number1 is: " << number1 << std::endl;
+	std::cout << "Memory Size of Number1 is: " << sizeof(number1) << " bytes" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Number2 is: " << number2 << std::endl;
+	std::cout << "Memory Size of Number2 is: " << sizeof(number2) << " bytes" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Number3 is: " << number3 << std::endl;
+	std::cout << "Memory Size of Number3 is: " << sizeof(number3) << " bytes" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Number4 is: " << number4 << std::endl;
+	std::cout << "Memory Size of Number4 is: " << sizeof(number4) << " bytes" << std::endl;
+
+	return 0;
 
 }
