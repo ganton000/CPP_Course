b78ef16cc12f858b0d7d24f5dd69ceaafe5a1a13
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-08 23:28:45 -0500

    Wrote celsius to fahrenheit script

---
+++ operations/exercise1.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <iostream>
+
+int convert_celsius_to_fahrenheit(double celsius) {
+
+	double const_var = (9.0/5);
+
+	return (const_var*celsius) + 32;
+}
+
+int main() {
+
+	std::cout << "Please enter a degree value in Celsius: " << std::endl;
+
+	double celsius;
+	std::cin >> celsius;
+
+	double result = convert_celsius_to_fahrenheit(celsius);
+
+	std::cout << celsius << " Celsius is " << result << " Fahrenheit" << std::endl;
+
+	return 0;
+}
