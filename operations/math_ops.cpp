ef54abf0712ed7ecc7c5a769b9e3f8ebd9a02175
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-08 23:02:08 -0500

    Added square_root function

--- operations/math_ops.cpp
+++ operations/math_ops.cpp
@@ -4,19 +4,26 @@
 
 int get_power(int num1, int num2) {
 
-	int power_res = std::pow(num1, num2);
-	return power_res;
+	return std::pow(num1, num2);
+}
+
+int get_square_root(int val) {
+	return std::sqrt(val);
 }
 
 int main() {
 
 
-	int num1 = 2;
-	int num2 = 4;
+	int num1 = 16;
+	int num2 = 2;
 
 	int result = get_power(num1, num2);
 
 	std::cout << num1 << " raised to the power of " << num2 << " equals " << result << std::endl;
+
+	int square_rooted = get_square_root(result);
+
+	std::cout << "The square root of " << result << " is " << square_rooted << std::endl;
 
 	return 0;
 }
