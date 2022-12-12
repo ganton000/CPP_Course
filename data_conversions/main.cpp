a5296269fba5a9a85ad9a132e83cc9b0e71921b2
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-12 13:16:34 -0500

    Added code for implicit and explicit casting

--- data_conversions/main.cpp
+++ data_conversions/main.cpp
@@ -36,5 +36,31 @@ int main() {
 
 	std::cout << "The value of x is: " << x << std::endl;
 
+	//Implicit cast will add up the doubles
+	// then turn result into int for assignment
+
+	double a { 12.5};
+	double b { 34.6 };
+
+	int sum = a + b;
+	std::cout << "The sum is: " << sum << std::endl;
+
+	//implicit cast:
+	sum = static_cast<int>(a) + static_cast<int>(b);
+
+	std::cout << "Cast then sum, result: " << sum << std::endl;
+
+	//explicit cast: sum up then cast, same as implicit cast
+	sum = static_cast<int> (a+b);
+
+	std::cout << "Explicit cast then sum, result: " << sum << std::endl;
+
+	//old style C-cast
+	//not recommended as static_cast<>() is clearer
+	//and checked by compiler, if types are not compatible
+	double PI {3.14};
+	int int_pi = (int)(PI);
+	std::cout << "int PI: " << int_pi << std::endl;
+
 	return 0;
 }
