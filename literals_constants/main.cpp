a3887a9f3ba193da558e24b181ffd409abf8523e
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-11 19:20:28 -0500

    Added code regarding const and constexpr

--- literals_constants/main.cpp
+++ literals_constants/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 
-//Literals don't have a location in memory, are burnt into the binary in code. So they're not stored into a variable, the variable would point to them
+//Literals don't have a location in memory, are burnt into the binary in code.
 
 int main() {
 
@@ -41,6 +41,43 @@ int main() {
 	int number_literal {15};
 	float fractional_literal {1.5f};
 	std::string string_literal {"Hit the road"};
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// constants can't be changed later on in script,
+	// will give compiler error if so
+	// const applies directly to the variable name itself and not at the memory level
+	// Hence the variable should be viewed as read-only
+
+	// If pointing another variable that is non-const
+	// at the same memory the const variable is located then
+	//could possible change the value
+
+	const unsigned int earth_radius_km {6371};
+
+	//const unsigned int earth_radius_km; //Compiler error
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//Constant expressions (constexpr) are evaluated at compile time
+
+	//So if possible, moving heavy computations at compile time
+	// will help users benefit from decreased runtime latency when running the application
+
+	//constexpr are also constants
+
+	constexpr int eye_count {2};
+	constexpr double PI {3.14};
+
+	int leg_count {2}; // Non constexpr
+	//As leg_count is not known at compile time but as runtime,
+	//the below code will throw an error
+	//constexpr int arm_count{leg_count}; //Error
+
+	//Checks at compile time
+	static_assert(eye_count == 2, "eye_count variable is not equal to 2");
+	static_assert(PI == 3.14, "PI variable is not equal to 3.14");
+
+
+
 
 
 	return 0;
