08d9878cb59bebbb78963763b1b5f4aac14214d4
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-18 13:17:36 -0500

    Added script for bitwise operations (bit masking)

---
+++ bitwise_operations/bit_mask.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <bitset>
+#include <iomanip>
+
+int main() {
+
+	//Highlights position for bit of interest with a 1
+	//Masks other positions with 0
+	const unsigned char mask_bit_0 {0b00000001}; //Bit0
+	const unsigned char mask_bit_1 {0b00000010}; //Bit1
+	const unsigned char mask_bit_2 {0b00000100}; //Bit2
+	const unsigned char mask_bit_3 {0b00001000}; //Bit3
+	const unsigned char mask_bit_4 {0b00010000}; //Bit4
+	const unsigned char mask_bit_5 {0b00100000}; //Bit5
+	const unsigned char mask_bit_6 {0b01000000}; //Bit6
+	const unsigned char mask_bit_7 {0b10000000}; //Bit7
+
+	/*
+	Can set/reset/check/toggle bit positions
+	*/
+
+	unsigned char var {0b00000000};
+	int COLUMN_WIDTH = 15;
+
+	//Setting Bits
+	//Setting: |= with mask of the bit
+
+	//Set bit 1
+	std::cout << "Setting bit in position 1" << std::endl;
+	var |= mask_bit_1;
+	std::cout << std::setw(COLUMN_WIDTH) << "var: " << std::setw(COLUMN_WIDTH) << std::bitset<8>(var) << std::endl;
+
+	//Set bit 5
+	std::cout << "Setting bit in position 5" << std::endl;
+	var |= mask_bit_5;
+	std::cout << std::setw(COLUMN_WIDTH) << "var: " << std::setw(COLUMN_WIDTH) << std::bitset<8>(var) << std::endl;
+
+	//Resetting bits
+
+	//Reset bit position 1 to 0
+	std::cout << "Resetting bit in position 1" << std::endl;
+	var &= (~mask_bit_1);
+	std::cout << std::setw(COLUMN_WIDTH) << "var: " << std::setw(COLUMN_WIDTH) << std::bitset<8>(var) << std::endl;
+
+	//Masking multiple bits at once
+	std::cout << "Setting all bits" << std::endl;
+	var |= ( mask_bit_0 | mask_bit_1 | mask_bit_2 | mask_bit_3 | mask_bit_4 | mask_bit_5 | mask_bit_6 | mask_bit_7 );
+	std::cout << std::setw(COLUMN_WIDTH) << "var: " << std::setw(COLUMN_WIDTH) << std::bitset<8>(var) << std::endl;
+
+
+	std::cout << "Reset bits at pos 0,2,4,6" << std::endl;
+	var &= ~( mask_bit_0 | mask_bit_2 | mask_bit_4 | mask_bit_6 );
+	std::cout << std::setw(COLUMN_WIDTH) << "var: " << std::setw(COLUMN_WIDTH) << std::bitset<8>(var) << std::endl;
+
+	return 0;
+}
