d19ca7d7971a67bb62be83b05e61f05dad26e5ae
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-18 19:06:06 -0500

    Add code dealing with if constxpr

--- control_flow/main.cpp
+++ control_flow/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
 
+constexpr bool condition {false};
+
 
 
 int main() {
 
+	//The branch which fails gets eliminated at compile time
+	if constexpr (condition) {
+		std::cout << "Condition is true" << std::endl;
+	} else {
+		std::cout << "Condition is not true" << std::endl;
+	}
 
 	return 0;
 }
