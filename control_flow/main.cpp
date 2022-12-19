b75e9318be7f9d127074abd851a8e986b503bd52
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-18 21:14:42 -0500

    Refactored code to use if constexpr statement and if statement with initializer

--- control_flow/main.cpp
+++ control_flow/main.cpp
@@ -4,15 +4,41 @@
 constexpr bool condition {false};
 
 
-
-int main() {
-
+int compile_time_if() {
 	//The branch which fails gets eliminated at compile time
 	if constexpr (condition) {
 		std::cout << "Condition is true" << std::endl;
 	} else {
 		std::cout << "Condition is not true" << std::endl;
 	}
+
+	return 0;
+}
+
+int if_with_initializer() {
+
+	bool go {true};
+
+	//declares and initializes variable inplace
+	if (int high_speed{33}; go) {
+		if (high_speed > 5){
+			std::cout << "Slow down!" << std::endl;
+		} else {
+			std::cout << "All good!" << std::endl;
+		}
+	} else {
+		std::cout << "high_speed: " << high_speed << std::endl;
+		std::cout << "Stop" << std::endl;
+	}
+
+	return 0;
+}
+
+int main() {
+
+	compile_time_if();
+
+	if_with_initializer();
 
 	return 0;
 }
