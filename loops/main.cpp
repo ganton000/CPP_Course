25877cee1b07e33d12c7f25abeb8ebabdb331b4a
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-24 18:49:36 -0500

    updated main to incorporate multiple declarations of for loop

--- loops/main.cpp
+++ loops/main.cpp
@@ -22,9 +22,17 @@ unsigned int sum_up_to_99(){
 	return sum;
 }
 
+void multiple_decl_for_loop() {
+
+	for (size_t i{0}, x{5}, y{22}; y > 15; ++i, x+=5, y-=1){
+		std::cout << "i: " << i << ", x: " << x << ", y: " << y << std::endl;
+	}
+}
+
 int main() {
 
 	basic_for_loop();
+	std::cout << std::endl;
 
 	std::cout << "sizeof(size_t): " << sizeof(size_t) << std::endl;
 	std::cout << "sizeof(unsigned int): " << sizeof(unsigned int) << std::endl;
@@ -32,6 +40,10 @@ int main() {
 	std::cout << "Sum of integers from 0 to 99 is: " << sum_up_to_99() << std::endl;
 
 	std::cout << "Algorithm sol: (99*100)/2 == " << ((99*100)/2) << std::endl;
+	std::cout << std::endl;
+
+	multiple_decl_for_loop();
+	std::cout << std::endl;
 
 	return 0;
 }
