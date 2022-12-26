8000eeeaa5db16e8c0c8ac2d6928764638e51825
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-25 23:10:19 -0500

    updated main cpp to get array size

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -1,16 +1,24 @@
+//Arrays store elements of the same type only
 #include <iostream>
-
-using namespace std;
 
 int main() {
 
 	int scores[10]; //Array storing 10 integers
 
-	cout << "scores[0]: " << scores[0] << endl;
-	cout << "scores[1]: " << scores[1] << endl;
-	cout << "scores[2]: " << scores[1] << endl;
-	cout << "scores[9]: " << scores[9] << endl;
+	std::cout << "scores[0]: " << scores[0] << std::endl;
+	std::cout << "scores[1]: " << scores[1] << std::endl;
+	std::cout << "scores[2]: " << scores[1] << std::endl;
+	std::cout << "scores[9]: " << scores[9] << std::endl;
 
+	//remaining elements are initialized to 0
+	int families[5] {12, 7, 5};
+
+	//constant arrays can't be modified
+	const int multipliers [] {1,2,5};
+
+	std::cout << "multipliers size: " << std::size(multipliers) << std::endl;
+
+	std::cout << "Old hack to get size of array: " << (sizeof(multipliers)/sizeof(multipliers[0])) << std::endl;
 
 	return 0;
 }
