143917c7e69fc3ee629fd61ee6dcacf25bb84574
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-26 15:35:21 -0500

    Added complete fortune teller program

--- arrays/fortune_teller.cpp
+++ arrays/fortune_teller.cpp
@@ -1,11 +1,58 @@
 #include <iostream>
 
 
+std::string get_message(int index) {
+
+	std::string message {};
+
+	switch(index) {
+
+		case 1:
+			message = " I see you laughing your lungs out. I've never seen this before";
+			break;
+		case 2:
+			message = " I see you running away from something really scary";
+			break;
+		case 3:
+			message = " I see you parting too much with people wearing weird clothes";
+			break;
+		case 4:
+			message = " I see pools of money scattered all over your backyard";
+			break;
+		case 5:
+			message = " I see many kids running in a backyard";
+			break;
+		case 6:
+			message = " I see you in a bugatti";
+			break;
+		case 7:
+			message = " I see dogs running around in a deserted empty city.";
+			break;
+		default:
+			message = " I see, hmmm..., I don't see anything.";
+			break;
+	}
+
+	return message;
+}
+
+int get_rand_num() {
+
+	std::srand(std::time(0));
+
+	return (std::rand()%9 +1);
+}
+
+
 void play_round(std::string name, char play_again) {
 
 	while ((play_again == 'Y') || (play_again == 'y')) {
 
-		std::cout << "Oh dear, " << name << "I see, hmmm..., I don't see anything." << std::endl;
+		int num = get_rand_num();
+
+		std::string message = get_message(num);
+
+		std::cout << name << ", oh dear... " << message << std::endl;
 
 		std::cout << "Would you like another fortune? ( Y | N ): " << std::endl;
 		std::cin >> play_again;
@@ -18,16 +65,11 @@ void play_round(std::string name, char play_again) {
 int main() {
 
 	std::string name{};
-	char play_again{};
+	char play_again {'Y'};
 
 	std::cout << "I see you have come to be told your fortune!" << std::endl;
 	std::cout << "Please tell me your name: " << std::endl;
 	std::cin >> name;
-
-	std::cout << "Oh dear, " << name << "I see dogs running around in a deserted empty city." << std::endl;
-
-	std::cout << "Would you like another fortune? ( Y | N ): " << std::endl;
-	std::cin >> play_again;
 
 	play_round(name, play_again);
 
