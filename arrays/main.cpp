f181deeb81351a4e5af5d467051425c4e20e63ca
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-25 22:25:47 -0500

    initial commit

---
+++ arrays/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int scores[10]; //Array storing 10 integers
+
+	cout << "scores[0]: " << scores[0] << endl;
+	cout << "scores[1]: " << scores[1] << endl;
+	cout << "scores[2]: " << scores[1] << endl;
+	cout << "scores[9]: " << scores[9] << endl;
+
+
+	return 0;
+}
