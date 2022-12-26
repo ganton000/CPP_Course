1a6700ecbd60cf84e26d0ce88b7b9b82a4ead63b
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-25 23:28:34 -0500

    added literal c strings

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -20,5 +20,25 @@ int main() {
 
 	std::cout << "Old hack to get size of array: " << (sizeof(multipliers)/sizeof(multipliers[0])) << std::endl;
 
+	//proper c-strings
+	char message[] {'H', 'e', 'l', 'l', 'o', '\0'};
+
+	std::cout << "message: " << message << std::endl;
+	std::cout << "size: " << std::size(message) << std::endl;
+
+	char message2[6] {'H', 'e', 'l', 'l', 'o'};
+
+	std::cout << "message: " << message2 << std::endl;
+	std::cout << "size: " << std::size(message2) << std::endl;
+
+	// defines a literal C-string; appends null character '\0'
+	// hence making it a c-string
+
+	char message3[] {"Hello World!"};
+
+	std::cout << "message: " << message3 << std::endl;
+	std::cout << "size: " << std::size(message3) << std::endl;
+
+
 	return 0;
 }
