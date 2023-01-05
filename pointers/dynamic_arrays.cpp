34fa019f82b2a5e2be8190db51a1844406fdcbc9
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-01-05 17:32:17 -0500

    updated dynamic arrays script

--- pointers/dynamic_arrays.cpp
+++ pointers/dynamic_arrays.cpp
@@ -5,8 +5,10 @@ int main() {
 
 	// Arrays allocated on the heap with the new operator
 
+
 	size_t size {10};
 
+	/* Different ways to declare an array dynamically */
 
 	// salaries array with contain garbage values
 	double *p_salaries { new double[size] };
@@ -17,6 +19,32 @@ int main() {
 	// Allocating memory space for an array of size double vars.
 	// First 5 initialized while rest will be 0's.
 	double *p_scores { new(std::nothrow) double[size]{1,2,3,4,5} };
+
+	// nullptr check
+	if (p_scores) {
+		for(size_t i{}; i < size; ++i) {
+			std::cout << "value: " << *(p_scores + i) << std::endl;
+		}
+	}
+
+	// Releasing memory
+	delete[] p_scores;
+	p_scores = nullptr;
+
+	delete[] p_students;
+	p_students = nullptr;
+
+	delete[] p_salaries;
+	p_salaries = nullptr;
+
+	/*
+	NOTE:
+	Pointers initialized with dynamic arrays differ from arrays:
+	std::size doesn't work on them and they don't support range based for loops.
+
+	We say that the dynamically allocated array has decayed into a pointer.
+	*/
+
 
 	return 0;
 }
